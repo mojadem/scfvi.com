fcec66ffc2741838c302676a298fab4b7cce350b
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-10-04 22:04:57 -0400

    begin migration to svelte

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-import PocketBase from "pocketbase";
-
-export const onRequest = defineMiddleware((context, next) => {
-  const pb = new PocketBase("https://scfvi.pockethost.io/") as TypedPocketBase;
-  context.locals.pb = pb;
-
-  return next();
-});
