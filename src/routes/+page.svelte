26fa81d986bf8f914c5111bf2fc31c450bb490b8
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-10-10 00:57:22 -0400

    color experimentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,27 +4,31 @@
 	export let data;
 </script>
 
-<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-	SAUDI CYBERFEMINISM VISUAL INDEX
-</h1>
+<div class="container mx-auto">
+	<header class="mt-4">
+		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+			SAUDI CYBERFEMINISM VISUAL INDEX
+		</h1>
+	</header>
 
-<Table.Root>
-	<Table.Header>
-		<Table.Row>
-			<Table.Head>Year</Table.Head>
-			<Table.Head>Title</Table.Head>
-			<Table.Head>Author</Table.Head>
-			<Table.Head>Campaign</Table.Head>
-		</Table.Row>
-	</Table.Header>
-	<Table.Body>
-		{#each data.entries as entry}
+	<Table.Root>
+		<Table.Header>
 			<Table.Row>
-				<Table.Cell>{entry.year}</Table.Cell>
-				<Table.Cell>{entry.title}</Table.Cell>
-				<Table.Cell>{entry.author}</Table.Cell>
-				<Table.Cell>{entry.expand['campaign'].name}</Table.Cell>
+				<Table.Head class="font-bold">Year</Table.Head>
+				<Table.Head class="font-bold">Title</Table.Head>
+				<Table.Head class="font-bold">Author</Table.Head>
+				<Table.Head class="font-bold">Campaign</Table.Head>
 			</Table.Row>
-		{/each}
-	</Table.Body>
-</Table.Root>
+		</Table.Header>
+		<Table.Body>
+			{#each data.entries as entry}
+				<Table.Row>
+					<Table.Cell>{entry.year}</Table.Cell>
+					<Table.Cell>{entry.title}</Table.Cell>
+					<Table.Cell>{entry.author}</Table.Cell>
+					<Table.Cell>{entry.expand['campaign'].name}</Table.Cell>
+				</Table.Row>
+			{/each}
+		</Table.Body>
+	</Table.Root>
+</div>
