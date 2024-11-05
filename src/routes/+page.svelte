977e75665c3bfe12af9627553542026d355dfc47
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-11-05 01:24:24 -0500

    update main page with search filtering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
+	import * as Sheet from '$lib/components/ui/sheet';
+	import * as Select from '$lib/components/ui/select';
 	import * as Pagination from '$lib/components/ui/pagination';
+	import { Input } from '$lib/components/ui/input';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import FilterSelection from '$lib/components/filter-selection.svelte';
 	import EntryCard from '$lib/components/entry-card.svelte';
+	import { Filter } from 'lucide-svelte';
 	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 
 	export let data;
 
+	const searchableFields = ['title', 'author'];
+
 	const allCampaigns = { value: 'all', label: 'All Campaigns' };
 	const allTypes = { value: 'all', label: 'All Content' };
-	const pageSize = 20;
+
+	const defaultSearchSelection = 'title';
 
 	let campaignSelection = allCampaigns;
 	let typeSelection = allTypes;
+	let searchSelection = defaultSearchSelection;
+
+	let searchString = '';
+
+	const pageSize = 20;
 	let currentPage = 1;
+	let isPageScrollable = true;
 
 	function resetFilters() {
 		campaignSelection = allCampaigns;
@@ -21,9 +35,11 @@
 	}
 
 	function scrollToTop() {
-		if (browser) {
-			window.scrollTo(0, 0);
+		if (!browser) {
+			return;
 		}
+
+		window.scrollTo(0, 0);
 	}
 
 	function resetPage() {
@@ -47,11 +63,40 @@
 			} else {
 				return entry.type === typeSelection.value;
 			}
+		})
+		.filter((entry) => {
+			if (searchSelection === 'title') {
+				return entry.title.toLowerCase().includes(searchString.toLowerCase());
+			} else if (searchSelection === 'author') {
+				return entry.author.toLowerCase().includes(searchString.toLowerCase());
+			}
 		});
 
 	$: pageEntries = filteredEntries.slice((currentPage - 1) * pageSize, currentPage * pageSize);
 
 	$: currentPage, scrollToTop();
+
+	// TODO: this sometimes refreshes sometimes doesn't, opening and closing the sheet has some effect
+
+	$: pageEntries,
+		(isPageScrollable = browser
+			? document.documentElement.scrollHeight > document.documentElement.clientHeight
+			: true);
+
+	onMount(() => {
+		const checkScrollable = () => {
+			isPageScrollable =
+				document.documentElement.scrollHeight > document.documentElement.clientHeight;
+		};
+
+		window.addEventListener('resize', checkScrollable);
+
+		checkScrollable();
+
+		return () => {
+			window.removeEventListener('resize', checkScrollable);
+		};
+	});
 </script>
 
 <div class="container mx-auto">
@@ -60,24 +105,57 @@
 			SAUDI CYBERFEMINISM VISUAL INDEX
 		</h1>
 		<div class="my-2"></div>
-		<div class="flex justify-start">
-			<FilterSelection
-				defaultSelection={allCampaigns}
-				bind:selected={campaignSelection}
-				items={data.campaigns}
-				class="w-[250px]"
-			/>
-			<div class="mx-2"></div>
+		<div class="flex">
+			<Input bind:value={searchString} placeholder={`Search by ${searchSelection}...`} />
+			<div class="mx-2" />
+			<Sheet.Root>
+				<Sheet.Trigger>
+					<Button class="aspect-square" variant="ghost">
+						<Filter size="32" />
+					</Button>
+				</Sheet.Trigger>
+				<Sheet.Content>
+					<Sheet.Header>
+						<Sheet.Title>Filter Entries</Sheet.Title>
+					</Sheet.Header>
+					<div class="my-2" />
+					<div class="grid w-full grid-cols-1 gap-2">
+						<FilterSelection
+							defaultSelection={allCampaigns}
+							bind:selected={campaignSelection}
+							items={data.campaigns}
+						/>
 
-			<FilterSelection
-				defaultSelection={allTypes}
-				bind:selected={typeSelection}
-				items={data.types}
-				class="w-[125px]"
-			/>
-			<div class="mx-2"></div>
+						<FilterSelection
+							defaultSelection={allTypes}
+							bind:selected={typeSelection}
+							items={data.types}
+						/>
 
-			<Button on:click={resetFilters}>Reset</Button>
+						<Select.Root
+							onSelectedChange={(selection) => {
+								if (selection === undefined) {
+									return;
+								}
+
+								searchSelection = selection.value;
+							}}
+							selected={{ value: searchSelection, label: `Search by ${searchSelection}` }}
+						>
+							<Select.Trigger>
+								<Select.Value />
+							</Select.Trigger>
+							<Select.Content>
+								{#each searchableFields as field}
+									<Select.Item value={field}>{`Search by ${field}`}</Select.Item>
+								{/each}
+							</Select.Content>
+						</Select.Root>
+
+						<Button on:click={resetFilters}>Reset</Button>
+					</div>
+				</Sheet.Content>
+			</Sheet.Root>
 		</div>
 	</header>
 
@@ -85,53 +163,57 @@
 	<div class="my-2"></div>
 
 	{#if filteredEntries.length > 0}
-		<Pagination.Root
-			count={filteredEntries.length}
-			perPage={pageSize}
-			bind:page={currentPage}
-			let:pages
-		>
-			<div class="grid grid-cols-1 gap-4 md:grid-cols-2" style="grid-template-rows: 1fr;">
-				{#each pageEntries as entry, index}
-					{#if index === 1}
-						<!-- column stagger -->
-						<div class="hidden md:block"></div>
-					{/if}
-					<div class="row-span-2">
-						<EntryCard {entry} bind:campaignSelection bind:typeSelection />
-					</div>
-				{/each}
-			</div>
+		<div class="grid grid-cols-1 gap-4 md:grid-cols-2" style="grid-template-rows: 1fr;">
+			{#each pageEntries as entry, index}
+				{#if index === 1}
+					<!-- column stagger -->
+					<div class="hidden md:block"></div>
+				{/if}
+				<div class="row-span-2">
+					<EntryCard {entry} bind:campaignSelection bind:typeSelection />
+				</div>
+			{/each}
+		</div>
+		<div class="my-2"></div>
+
+		{#if filteredEntries.length > pageSize}
+			<Pagination.Root
+				count={filteredEntries.length}
+				perPage={pageSize}
+				bind:page={currentPage}
+				let:pages
+			>
+				<Pagination.Content>
+					<Pagination.Item>
+						<Pagination.PrevButton />
+					</Pagination.Item>
+					{#each pages as page (page.key)}
+						{#if page.type === 'ellipsis'}
+							<Pagination.Item>
+								<Pagination.Ellipsis />
+							</Pagination.Item>
+						{:else}
+							<Pagination.Item isVisible={currentPage == page.value}>
+								<Pagination.Link {page} isActive={currentPage == page.value}>
+									{page.value}
+								</Pagination.Link>
+							</Pagination.Item>
+						{/if}
+					{/each}
+					<Pagination.Item>
+						<Pagination.NextButton />
+					</Pagination.Item>
+				</Pagination.Content>
+			</Pagination.Root>
+		{/if}
+
+		{#if isPageScrollable}
 			<div class="my-2"></div>
+			<div class="flex justify-center">
+				<button class="text-sm underline" on:click={scrollToTop}>Back to top</button>
+			</div>
+		{/if}
 
-			<Pagination.Content>
-				<Pagination.Item>
-					<Pagination.PrevButton />
-				</Pagination.Item>
-				{#each pages as page (page.key)}
-					{#if page.type === 'ellipsis'}
-						<Pagination.Item>
-							<Pagination.Ellipsis />
-						</Pagination.Item>
-					{:else}
-						<Pagination.Item isVisible={currentPage == page.value}>
-							<Pagination.Link {page} isActive={currentPage == page.value}>
-								{page.value}
-							</Pagination.Link>
-						</Pagination.Item>
-					{/if}
-				{/each}
-				<Pagination.Item>
-					<Pagination.NextButton />
-				</Pagination.Item>
-			</Pagination.Content>
-		</Pagination.Root>
+		<div class="my-4"></div>
 	{/if}
-
-	<div class="my-2"></div>
-	<div class="flex justify-center">
-		<button class="text-sm underline" on:click={scrollToTop}>Back to top</button>
-	</div>
-
-	<div class="my-4"></div>
 </div>
