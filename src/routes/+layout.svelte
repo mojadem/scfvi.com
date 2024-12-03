ffee34300139aa47cec88c41f5e9d5e72f44d8c0
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-12-03 09:48:20 -0500

    add link to header text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,11 @@
 
 <div class="container mx-auto">
 	<header class="my-4">
-		<h1 class="scroll-m-20 font-display text-4xl font-extrabold tracking-tight lg:text-5xl">
-			SAUDI CYBERFEMINISM VISUAL INDEX
-		</h1>
+		<a href="/" target="_self">
+			<h1 class="scroll-m-20 font-display text-4xl font-extrabold tracking-tight lg:text-5xl">
+				SAUDI CYBERFEMINISM VISUAL INDEX
+			</h1>
+		</a>
 	</header>
 	<slot></slot>
 </div>
