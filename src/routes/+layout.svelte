41819eb506c4a85c895ddb4f07b8b97aba7b7502
Matt Dembiczak <dembiczakmatt@gmail.com>
2025-01-16 16:39:16 -0500

    re-init project for svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,6 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	let { children } = $props();
 </script>
 
-<div class="container mx-auto flex min-h-screen flex-col">
-	<header class="my-4">
-		<a href="/" target="_self">
-			<h1 class="scroll-m-20 font-display text-4xl font-extrabold tracking-tight lg:text-5xl">
-				SAUDI CYBERFEMINISM VISUAL INDEX
-			</h1>
-		</a>
-	</header>
-	<slot></slot>
-	<div class="flex-grow" />
-	<footer class="py-4">
-		<div class="flex items-center justify-center">
-			<a href="/about" class="hover:underline">about</a>
-			<span class="px-1 py-1">|</span>
-			<a href="/disclaimer" class="hover:underline">disclaimer</a>
-			<span class="px-1 py-1">|</span>
-			<a href="/submit" class="hover:underline">submit</a>
-			<span class="px-1 py-1">|</span>
-			<a href="/" class="hover:underline">home</a>
-		</div>
-	</footer>
-</div>
+{@render children()}
