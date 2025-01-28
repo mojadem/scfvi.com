fd9001cfb9a81de13931708a41df6bfdb7ad6075
Matt Dembiczak <matt@mojadem.dev>
2025-01-28 08:58:13 -0500

    add logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo.svg';
+</script>
+
 <main class="container mx-auto">
-	<h1 class="scroll-m-20 py-4 font-display text-4xl font-extrabold tracking-tight lg:text-5xl">
-		SCFVI
-	</h1>
+	<header class="py-4">
+		<img src={logo} alt="SCFVI logo" class="w-32" />
+	</header>
 	<p>This site is under construction...</p>
 </main>
