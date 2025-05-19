be1ccd97ed9c15b601776dcbc909955104c3c21c
Matt Dembiczak <matt@dembiczak.net>
2025-05-19 18:05:30 -0400

    client: remove unused import

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -1,4 +1,4 @@
-import { atom, computed, effect, map } from "nanostores";
+import { atom, computed, map } from "nanostores";
 import type { Entry } from "@lib/types.ts";
 import { defaultFilters, filterEntries, type Filters } from "@lib/filter.ts";
 
