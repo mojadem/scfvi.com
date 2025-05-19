020f0e8f29c4faead02b4c53416bec78bee7965d
Matt Dembiczak <matt@dembiczak.net>
2025-05-19 18:45:34 -0400

    client: filter search fields case insensitively

--- client/src/lib/filter.ts
+++ client/src/lib/filter.ts
@@ -19,12 +19,16 @@ export const defaultFilters: Filters = {
 export type FiltersStringKey = keyof Pick<Filters, "title" | "author" | "year">;
 export type FiltersArrayKey = keyof Pick<Filters, "campaigns" | "types">;
 
+function checkForSubstring(str: string, substr: string): boolean {
+  return str.toLowerCase().includes(substr.toLowerCase());
+}
+
 export function filterEntries(entries: Entry[], filters: Filters): Entry[] {
   return entries.filter(
     (entry) =>
-      entry.title.includes(filters.title) &&
-      entry.author.includes(filters.author) &&
-      entry.year.toString().includes(filters.year) &&
+      checkForSubstring(entry.title, filters.title) &&
+      checkForSubstring(entry.author, filters.author) &&
+      checkForSubstring(entry.year.toString(), filters.year) &&
       entry.expand.campaign.some(
         (campaign) => !filters.campaigns.includes(campaign.name),
       ) &&
