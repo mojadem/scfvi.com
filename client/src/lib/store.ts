8c4cb76b75f496c5dd7ebfdfdb2105be8547f99e
Matt Dembiczak <matt@dembiczak.net>
2025-05-21 14:22:18 -0400

    client: embed all data in entry list

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -1,9 +1,12 @@
 import { atom, computed, map } from "nanostores";
-import type { Entry } from "@lib/types.ts";
+import { type Type, type Campaign, type Entry } from "@lib/types.ts";
 import { defaultFilters, filterEntries, type Filters } from "@lib/filter.ts";
 
-export const filters = map<Filters>(defaultFilters);
 export const entries = atom<Entry[]>([]);
+export const campaigns = atom<Campaign[]>([]);
+export const types = atom<Type[]>([]);
+
+export const filters = map<Filters>(defaultFilters);
 
 export const filteredEntries = computed(
   [entries, filters],
