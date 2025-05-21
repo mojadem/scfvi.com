53e2cce9bdd17a294b9622aaff46815cbbe0d781
Matt Dembiczak <matt@dembiczak.net>
2025-05-21 12:54:11 -0400

    client: add button to toggle all array filters

---
+++ client/src/lib/components/FilterToggleAll.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { FiltersArrayKey } from "@lib/filter.ts";
+  import { filters } from "@lib/store.ts";
+
+  let { key, values }: { key: FiltersArrayKey; values: string[] } = $props();
+
+  function updateFilters() {
+    if (filters.get()[key].length) {
+      filters.setKey(key, []);
+    } else {
+      filters.setKey(key, [...values]);
+    }
+  }
+</script>
+
+<button onclick={updateFilters}> toggle all </button>
+
+<style>
+  button {
+    display: block;
+    margin-top: 1rem;
+  }
+</style>
